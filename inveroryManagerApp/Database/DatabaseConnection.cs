181d7ef429380a7ea315fdcb1c83dcf0227b8e76
Raaga Krishna <raaga@bbd.co.za>
2023-04-21 06:01:47 +0200

    Fixed typos

--- inveroryManagerApp/Database/DatabaseConnection.cs
+++ inveroryManagerApp/Database/DatabaseConnection.cs
@@ -7,8 +7,9 @@ public partial class DatabaseConnection
 {
     private SqlConnection ConnectToDatabase()
     {
-        string connectionString = @"Server=KaydenK\SQLEXPRESS;Database=invetory_manager;Trusted_Connection=True;MultipleActiveResultSets=true";
+        //string connectionString = @"Server=KaydenK\SQLEXPRESS;Database=invetory_manager;Trusted_Connection=True;MultipleActiveResultSets=true";
         //string connectionString = @"Data Source=.\DEVLON_LOCAL;Initial Catalog=invetory_manager;Trusted_Connection=True;Integrated Security=True";
+        string connectionString = @"Server=(local);Database=invetory_manager;Trusted_Connection=True;MultipleActiveResultSets=true";
 
         SqlConnection conn = new SqlConnection(connectionString);
         conn.Open();
