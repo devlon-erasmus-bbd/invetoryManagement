9c15d0bc4cc6d136cc5c7eee015b4aafaff532fe
Raaga Krishna <raaga@bbd.co.za>
2023-04-21 06:23:00 +0200

    CustomersController (WIP)

---
+++ inveroryManagerApp/Controllers/CustomersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using inveroryManagerApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace inveroryManagerApp.Controllers;
+
+public partial class CustomersController : Controller
+{
+    
+}
